875b6b321703ebc9f62a115e74e9dff200d68de8
Malindu Prabodhitha <eamalindu@gmail.com>
2024-10-11 09:23:12 +0530

    Add files via upload
    CPU Temp and Fan Speed will update every 2 seconds

--- AsusFanControlGUI/Form1.Designer.cs
+++ AsusFanControlGUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.trackBarFanSpeed = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
@@ -44,16 +45,18 @@
             this.toolStripMenuItemTurnOffControlOnExit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemForbidUnsafeSettings = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemCheckForUpdates = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.trackBarFanSpeed)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // trackBarFanSpeed
             // 
-            this.trackBarFanSpeed.Location = new System.Drawing.Point(12, 62);
+            this.trackBarFanSpeed.Location = new System.Drawing.Point(16, 76);
+            this.trackBarFanSpeed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarFanSpeed.Maximum = 100;
             this.trackBarFanSpeed.Name = "trackBarFanSpeed";
-            this.trackBarFanSpeed.Size = new System.Drawing.Size(300, 45);
+            this.trackBarFanSpeed.Size = new System.Drawing.Size(400, 56);
             this.trackBarFanSpeed.TabIndex = 0;
             this.trackBarFanSpeed.Value = 100;
             this.trackBarFanSpeed.KeyUp += new System.Windows.Forms.KeyEventHandler(this.trackBarFanSpeed_KeyUp);
@@ -62,35 +65,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Location = new System.Drawing.Point(16, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Current value:";
             // 
             // labelValue
             // 
             this.labelValue.AutoSize = true;
-            this.labelValue.Location = new System.Drawing.Point(91, 110);
+            this.labelValue.Location = new System.Drawing.Point(121, 135);
+            this.labelValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelValue.Name = "labelValue";
-            this.labelValue.Size = new System.Drawing.Size(10, 13);
+            this.labelValue.Size = new System.Drawing.Size(11, 16);
             this.labelValue.TabIndex = 2;
             this.labelValue.Text = "-";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 139);
+            this.label2.Location = new System.Drawing.Point(53, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.Size = new System.Drawing.Size(85, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Current RPM:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 134);
+            this.button1.Location = new System.Drawing.Point(16, 165);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.Size = new System.Drawing.Size(29, 28);
             this.button1.TabIndex = 4;
             this.button1.Text = "↻";
             this.button1.UseVisualStyleBackColor = true;
@@ -99,18 +106,20 @@
             // labelRPM
             // 
             this.labelRPM.AutoSize = true;
-            this.labelRPM.Location = new System.Drawing.Point(117, 139);
+            this.labelRPM.Location = new System.Drawing.Point(156, 171);
+            this.labelRPM.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRPM.Name = "labelRPM";
-            this.labelRPM.Size = new System.Drawing.Size(10, 13);
+            this.labelRPM.Size = new System.Drawing.Size(11, 16);
             this.labelRPM.TabIndex = 5;
             this.labelRPM.Text = "-";
             // 
             // checkBoxTurnOn
             // 
             this.checkBoxTurnOn.AutoSize = true;
-            this.checkBoxTurnOn.Location = new System.Drawing.Point(12, 37);
+            this.checkBoxTurnOn.Location = new System.Drawing.Point(16, 46);
+            this.checkBoxTurnOn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxTurnOn.Name = "checkBoxTurnOn";
-            this.checkBoxTurnOn.Size = new System.Drawing.Size(116, 17);
+            this.checkBoxTurnOn.Size = new System.Drawing.Size(138, 20);
             this.checkBoxTurnOn.TabIndex = 6;
             this.checkBoxTurnOn.Text = "Turn on fan control";
             this.checkBoxTurnOn.UseVisualStyleBackColor = true;
@@ -119,17 +128,19 @@
             // labelCPUTemp
             // 
             this.labelCPUTemp.AutoSize = true;
-            this.labelCPUTemp.Location = new System.Drawing.Point(141, 168);
+            this.labelCPUTemp.Location = new System.Drawing.Point(188, 207);
+            this.labelCPUTemp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCPUTemp.Name = "labelCPUTemp";
-            this.labelCPUTemp.Size = new System.Drawing.Size(10, 13);
+            this.labelCPUTemp.Size = new System.Drawing.Size(11, 16);
             this.labelCPUTemp.TabIndex = 9;
             this.labelCPUTemp.Text = "-";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 163);
+            this.button2.Location = new System.Drawing.Point(16, 201);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(22, 23);
+            this.button2.Size = new System.Drawing.Size(29, 28);
             this.button2.TabIndex = 8;
             this.button2.Text = "↻";
             this.button2.UseVisualStyleBackColor = true;
@@ -138,20 +149,22 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(40, 168);
+            this.label4.Location = new System.Drawing.Point(53, 207);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.Size = new System.Drawing.Size(116, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = "Current CPU temp:";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1,
             this.toolStripMenuItemCheckForUpdates});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(324, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(432, 28);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -161,14 +174,14 @@
             this.toolStripMenuItemTurnOffControlOnExit,
             this.toolStripMenuItemForbidUnsafeSettings});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(72, 20);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(89, 24);
             this.toolStripMenuItem1.Text = "Advanced";
             // 
             // toolStripMenuItemTurnOffControlOnExit
             // 
             this.toolStripMenuItemTurnOffControlOnExit.CheckOnClick = true;
             this.toolStripMenuItemTurnOffControlOnExit.Name = "toolStripMenuItemTurnOffControlOnExit";
-            this.toolStripMenuItemTurnOffControlOnExit.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItemTurnOffControlOnExit.Size = new System.Drawing.Size(244, 26);
             this.toolStripMenuItemTurnOffControlOnExit.Text = "Turn off control on exit";
             this.toolStripMenuItemTurnOffControlOnExit.CheckedChanged += new System.EventHandler(this.toolStripMenuItemTurnOffControlOnExit_CheckedChanged);
             // 
@@ -176,22 +189,22 @@
             // 
             this.toolStripMenuItemForbidUnsafeSettings.CheckOnClick = true;
             this.toolStripMenuItemForbidUnsafeSettings.Name = "toolStripMenuItemForbidUnsafeSettings";
-            this.toolStripMenuItemForbidUnsafeSettings.Size = new System.Drawing.Size(196, 22);
+            this.toolStripMenuItemForbidUnsafeSettings.Size = new System.Drawing.Size(244, 26);
             this.toolStripMenuItemForbidUnsafeSettings.Text = "Forbid unsafe settings";
             this.toolStripMenuItemForbidUnsafeSettings.CheckedChanged += new System.EventHandler(this.toolStripMenuItemForbidUnsafeSettings_CheckedChanged);
             // 
             // toolStripMenuItemCheckForUpdates
             // 
             this.toolStripMenuItemCheckForUpdates.Name = "toolStripMenuItemCheckForUpdates";
-            this.toolStripMenuItemCheckForUpdates.Size = new System.Drawing.Size(115, 20);
+            this.toolStripMenuItemCheckForUpdates.Size = new System.Drawing.Size(142, 24);
             this.toolStripMenuItemCheckForUpdates.Text = "Check for updates";
             this.toolStripMenuItemCheckForUpdates.Click += new System.EventHandler(this.toolStripMenuItemCheckForUpdates_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 198);
+            this.ClientSize = new System.Drawing.Size(432, 244);
             this.Controls.Add(this.labelCPUTemp);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
@@ -205,8 +218,10 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Asus Fan Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBarFanSpeed)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -232,6 +247,7 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemTurnOffControlOnExit;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemForbidUnsafeSettings;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCheckForUpdates;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
